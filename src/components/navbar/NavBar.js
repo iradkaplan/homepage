f77d3a4505aa457a0c00fadb11126d98dfcfaa4e
Ira Kaplan <iradkaplan@gmail.com>
2019-01-19 22:13:07 -0800

    decent navbar styling.

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -3,11 +3,16 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { Link } from "react-router-dom";
 
 const styles = {
   root: {
+    flexGrow: 1,
+  },
+  title: {
+    textAlign: "left",
     flexGrow: 1,
   },
 };
@@ -19,10 +24,15 @@ function NavBar(props) {
     <div className={classes.root}>
       <AppBar position="fixed" color="default">
         <Toolbar>
-          <Typography variant="h6" color="inherit">
-            <Link to="/">Home</Link>
-            <Link to="/resume/">Resume</Link>
+          <Typography variant="h6" color="inherit" className={classes.title} >
+          Ira Kaplan
           </Typography>
+            <Link to="/">
+              <Button>Home</Button>
+            </Link>
+            <Link to="/resume/">
+              <Button>Resume</Button>
+            </Link>
         </Toolbar>
       </AppBar>
     </div>
