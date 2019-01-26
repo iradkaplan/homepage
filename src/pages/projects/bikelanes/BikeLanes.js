d153892bb6d471fef395d872aeb1ab753273accc
Ira Kaplan <iradkaplan@gmail.com>
2019-01-26 11:17:46 -0800

    added a map to bikelanes page

--- src/pages/projects/bikelanes/BikeLanes.js
+++ src/pages/projects/bikelanes/BikeLanes.js
@@ -1,22 +1,42 @@
 import React, { Component } from "react";
 import Grid from "@material-ui/core/Grid";
+import DeckGL, { LineLayer } from "deck.gl";
+import { StaticMap } from "react-map-gl";
 import "./BikeLanes.css";
+
+const MAPBOX_ACCESS_TOKEN = process.env["REACT_APP_MAPBOX_TOKEN"];
+
+// Initial viewport settings
+const initialViewState = {
+  longitude: -122.41669,
+  latitude: 37.7853,
+  zoom: 13,
+  pitch: 0,
+  bearing: 0
+};
+
+// Data to be used by the LineLayer
+const data = [
+  {
+    sourcePosition: [-122.41669, 37.7853],
+    targetPosition: [-122.41669, 37.781]
+  }
+];
 
 class BikeLanes extends Component {
   render() {
+    const layers = [new LineLayer({ id: "line-layer", data })];
     return (
       <>
+        {console.log(MAPBOX_ACCESS_TOKEN)}
         <Grid item xs={12} className="navbar-shim" />
-        <div className="App-body">
-          <Grid
-            container
-            direction="row"
-            justify="space-evenly"
-            alignItems="center"
-          >
-            <Grid item />
-          </Grid>
-        </div>
+        <DeckGL
+          initialViewState={initialViewState}
+          controller={true}
+          layers={layers}
+        >
+          <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
+        </DeckGL>
       </>
     );
   }
