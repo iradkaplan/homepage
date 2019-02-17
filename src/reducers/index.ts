207cae85a544bf8c1aa0a8e3209661771fd10339
Ira Kaplan <iradkaplan@gmail.com>
2019-02-17 01:02:53 -0800

    renamed actions

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -2,7 +2,7 @@ import { Reducer, combineReducers } from "redux";
 import * as actions from "../actions";
 import { ActionType } from "typesafe-actions";
 
-export type NavBarAction = ActionType<typeof actions>;
+export type AppAction = ActionType<typeof actions>;
 
 export interface NavBarState {
   projectMenuAnchor: null | ((element: HTMLElement) => HTMLElement);
@@ -14,7 +14,7 @@ const initialState: NavBarState = {
 
 const navbarReducer: Reducer<NavBarState> = (
   state: NavBarState = initialState,
-  action: NavBarAction
+  action: AppAction
 ) => {
   switch (action.type) {
     case actions.OPEN_MENU:
@@ -29,7 +29,7 @@ const navbarReducer: Reducer<NavBarState> = (
 
 export interface BasicState {}
 
-const basicReducer: Reducer<BasicState> = (state = {}, action: any) => {
+const basicReducer: Reducer<BasicState> = (state = {}, action: AppAction) => {
   // For now, don't handle any actions
   // and just return the state given to us.
   return state;
