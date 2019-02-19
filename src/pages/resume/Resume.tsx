b6fd612dbab5f4fc5b6b2e4e31a6566e2eee0a8d
Ira Kaplan <iradkaplan@gmail.com>
2019-02-18 19:45:07 -0800

    Restyled resume page

--- src/pages/resume/Resume.tsx
+++ src/pages/resume/Resume.tsx
@@ -13,10 +13,23 @@ class Resume extends Component {
         <div className="App-body">
           <Grid
             container
-            direction="row"
-            justify="space-evenly"
-            alignItems="center"
+            direction="column"
+            justify="flex-start"
+            alignItems="flex-start"
           >
+            <Grid item xs={12} sm={10} md={8} lg={6} xl={4}>
+              <Typography variant="body1">
+                Here you can view my resume, or click the image to open a PDF.
+                You can also{" "}
+                <Link
+                  to="//linkedin.com/in/irakaplan"
+                  target="_blank"
+                  className="link"
+                >
+                  find me on LinkedIn.
+                </Link>
+              </Typography>
+            </Grid>
             <Grid item>
               <Paper className="image-paper">
                 <Link
@@ -29,18 +42,7 @@ class Resume extends Component {
                     className="Resume-image"
                     alt="My resume. Click to open PDF."
                   />
-                  <Typography variant="caption" paragraph>
-                    Click to open PDF.
-                  </Typography>
-                </Link>
-                <Link
-                  to="//linkedin.com/in/irakaplan"
-                  target="_blank"
-                  className="link"
-                >
-                  <Typography variant="caption">
-                    Or view my LinkedIn profile.
-                  </Typography>
+                  <Typography variant="caption">Click to open PDF.</Typography>
                 </Link>
               </Paper>
             </Grid>
