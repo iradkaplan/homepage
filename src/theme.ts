ce9becb7f23db9153dde61f33d92185a732d3158
Ira Kaplan <iradkaplan@gmail.com>
2019-02-18 19:45:21 -0800

    organized fonts

--- src/theme.ts
+++ src/theme.ts
@@ -3,6 +3,7 @@ import createMuiTheme, {
   Theme
 } from "@material-ui/core/styles/createMuiTheme";
 import { Breakpoint } from "@material-ui/core/styles/createBreakpoints";
+import { CSSProperties } from "@material-ui/core/styles/withStyles";
 
 declare module "@material-ui/core/styles/createMuiTheme" {
   interface Theme {
@@ -11,6 +12,7 @@ declare module "@material-ui/core/styles/createMuiTheme" {
       breakpoint: Breakpoint;
     };
   }
+
   // allow configuration using `createMuiTheme`
   interface ThemeOptions {
     appDrawer?: {
@@ -20,7 +22,41 @@ declare module "@material-ui/core/styles/createMuiTheme" {
   }
 }
 
+declare module "@material-ui/core/styles/createTypography" {
+  interface Typography {
+    monoFonts: CSSProperties["fontFamily"];
+    sansSerifFonts: CSSProperties["fontFamily"];
+  }
+  interface TypographyOptions {
+    monoFonts?: CSSProperties["fontFamily"];
+    sansSerifFonts?: CSSProperties["fontFamily"];
+  }
+}
+
 export default function createTheme(options?: ThemeOptions) {
+  let monoFonts: string = [
+    "Source Code Pro",
+    "source-code-pro",
+    "Menlo",
+    "Monaco",
+    "Consolas",
+    "Courier New",
+    "monospace"
+  ].join(",");
+  let sansSerifFonts: string = [
+    "Open Sans",
+    "-apple-system",
+    "BlinkMacSystemFont",
+    "Segoe UI",
+    "Roboto",
+    "Oxygen",
+    "Ubuntu",
+    "Cantarell",
+    "Fira Sans",
+    "Droid Sans",
+    "Helvetica Neue",
+    "sans-serif"
+  ].join(",");
   return createMuiTheme({
     appDrawer: {
       width: 225,
@@ -43,14 +79,16 @@ export default function createTheme(options?: ThemeOptions) {
     },
     typography: {
       useNextVariants: true,
-      fontFamily: "Open Sans",
-      caption: { fontFamily: "Open Sans", textAlign: "center" },
-      h1: { fontFamily: "Source Code Pro", textAlign: "left" },
-      h2: { fontFamily: "Source Code Pro", textAlign: "left" },
-      h3: { fontFamily: "Source Code Pro", textAlign: "left" },
-      h4: { fontFamily: "Source Code Pro", textAlign: "left" },
-      h5: { fontFamily: "Source Code Pro", textAlign: "left" },
-      h6: { fontFamily: "Source Code Pro", textAlign: "left" },
+      monoFonts: monoFonts,
+      sansSerifFonts: sansSerifFonts,
+      fontFamily: sansSerifFonts,
+      caption: { fontFamily: sansSerifFonts, textAlign: "center" },
+      h1: { fontFamily: monoFonts, textAlign: "left" },
+      h2: { fontFamily: monoFonts, textAlign: "left" },
+      h3: { fontFamily: monoFonts, textAlign: "left" },
+      h4: { fontFamily: monoFonts, textAlign: "left" },
+      h5: { fontFamily: monoFonts, textAlign: "left" },
+      h6: { fontFamily: monoFonts, textAlign: "left" },
       body1: { fontSize: 16, textAlign: "left" /*, color: "white" */ },
       body2: {
         fontSize: 12,
