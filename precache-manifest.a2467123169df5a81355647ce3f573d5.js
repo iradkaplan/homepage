4890ce3474a9b808a4303f3ec3faa1a364185d88
Ira Kaplan <iradkaplan@gmail.com>
2021-08-08 14:13:23 -0700

    make the build folder the entire thing

---
+++ precache-manifest.a2467123169df5a81355647ce3f573d5.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "876ec2df1793a411d5ad",
+    "url": "/homepage/static/css/main.193daec3.chunk.css"
+  },
+  {
+    "revision": "876ec2df1793a411d5ad",
+    "url": "/homepage/static/js/main.9d598116.chunk.js"
+  },
+  {
+    "revision": "fb443e854746e2162a41",
+    "url": "/homepage/static/js/runtime~main.fca01a22.js"
+  },
+  {
+    "revision": "5cdd7ba571918b2c4890",
+    "url": "/homepage/static/js/2.d6587442.chunk.js"
+  },
+  {
+    "revision": "ccfca6742880ec11f356",
+    "url": "/homepage/static/css/3.da445c99.chunk.css"
+  },
+  {
+    "revision": "ccfca6742880ec11f356",
+    "url": "/homepage/static/js/3.ce52072e.chunk.js"
+  },
+  {
+    "revision": "eedd9323cf8586c684b2",
+    "url": "/homepage/static/css/4.0f274346.chunk.css"
+  },
+  {
+    "revision": "eedd9323cf8586c684b2",
+    "url": "/homepage/static/js/4.c18f000a.chunk.js"
+  },
+  {
+    "revision": "cc960321e24642c25d9a",
+    "url": "/homepage/static/css/5.88d70bfe.chunk.css"
+  },
+  {
+    "revision": "cc960321e24642c25d9a",
+    "url": "/homepage/static/js/5.f3fcb388.chunk.js"
+  },
+  {
+    "revision": "f3c11c7d74dce70f0e3e",
+    "url": "/homepage/static/css/6.4a6ce6b7.chunk.css"
+  },
+  {
+    "revision": "f3c11c7d74dce70f0e3e",
+    "url": "/homepage/static/js/6.9e959658.chunk.js"
+  },
+  {
+    "revision": "432064088171774443a6",
+    "url": "/homepage/static/css/7.055a2eb6.chunk.css"
+  },
+  {
+    "revision": "432064088171774443a6",
+    "url": "/homepage/static/js/7.42bf808e.chunk.js"
+  },
+  {
+    "revision": "3dfa2a160451d2268f32",
+    "url": "/homepage/static/css/8.7b196b61.chunk.css"
+  },
+  {
+    "revision": "3dfa2a160451d2268f32",
+    "url": "/homepage/static/js/8.a65782b0.chunk.js"
+  },
+  {
+    "revision": "33ce848826d92e0d42f35f00129ac684",
+    "url": "/homepage/static/media/photo.33ce8488.jpg"
+  },
+  {
+    "revision": "76c15314198c091d62068b7f3c845904",
+    "url": "/homepage/static/media/Ira Kaplan Resume.76c15314.png"
+  },
+  {
+    "revision": "44205c086d0c2a4b38a953e54e8eff09",
+    "url": "/homepage/static/media/homepage_screenshot.44205c08.png"
+  },
+  {
+    "revision": "66b9970a388516b10d52fd7897051327",
+    "url": "/homepage/static/media/sankey_screenshot_june5.66b9970a.png"
+  },
+  {
+    "revision": "5892a9aec338ce03ccff84f94535b831",
+    "url": "/homepage/static/media/sankey_screenshot_june13.5892a9ae.png"
+  },
+  {
+    "revision": "edd702c81f096b117dc89ceac4ad0492",
+    "url": "/homepage/static/media/SFBikeMap.edd702c8.png"
+  },
+  {
+    "revision": "8c6abb13fb8f8b85dcbe2b02751e622f",
+    "url": "/homepage/static/media/GoogleBikeMap.8c6abb13.png"
+  },
+  {
+    "revision": "cf5a7e38ef435af5a62b57197adad49b",
+    "url": "/homepage/static/media/LondonBikePlan.cf5a7e38.png"
+  },
+  {
+    "revision": "a28463a7a39986068632b835fc1a699e",
+    "url": "/homepage/static/media/SevilleBikeMap.a28463a7.png"
+  },
+  {
+    "revision": "29c8fe246feee4e0be0e5d299852c62a",
+    "url": "/homepage/static/media/SFBikePlan.29c8fe24.png"
+  },
+  {
+    "revision": "ea9ecffc491f4c2a05103dca1226d83c",
+    "url": "/homepage/index.html"
+  }
+];
